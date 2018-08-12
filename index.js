67a90365a2241b11bd9f3ac924c845a832e69d76
Jbudny <jbudny8@gmail.com>
2018-08-12 22:29:34 +0200

    Count images in the gallery

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
+const fs = require('file-system');
+const dir = './public/assets/images';
 
 app.get('/', (req, res) => {res.sendFile(__dirname + '/views/index.html');})
 app.use('/static/', express.static(__dirname + '/public/css'))
 app.use('/static/', express.static(__dirname + '/public/assets/images'))
 
 app.listen(3002, () => console.log('Example app listening on port 3000!'))
+
+fs.readdir(dir, (err, files) => {
+  console.log(files.length);
+});
