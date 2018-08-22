effd99dba74038b531b6630c68532196eccb4ee9
Jbudny <jbudny8@gmail.com>
2018-08-22 02:33:36 +0200

    Add modals functionality and update style

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const app = express();
 const fs = require('file-system');
-const dir = './public/assets/images';
+const dir = './public/assets/images/gallery';
 const $ = require('jQuery');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const chokidar = require('chokidar');
 const allowedExtensions = [
-  './public/assets/images/*.jpeg',
-  './public/assets/images/*.jpg',
-  './public/assets/images/*.jpe',
-  './public/assets/images/*.jif',
-  './public/assets/images/*.jfif',
-  './public/assets/images/*.jfi',
-  './public/assets/images/*.png',
-  './public/assets/images/*.gif',
-  './public/assets/images/*.bmp',
-  './public/assets/images/*.svg',
-  './public/assets/images/*.ico',
-  './public/assets/images/*.dib'
+  './public/assets/images/gallery/*.jpeg',
+  './public/assets/images/gallery/*.jpg',
+  './public/assets/images/gallery/*.jpe',
+  './public/assets/images/gallery/*.jif',
+  './public/assets/images/gallery/*.jfif',
+  './public/assets/images/gallery/*.jfi',
+  './public/assets/images/gallery/*.png',
+  './public/assets/images/gallery/*.gif',
+  './public/assets/images/gallery/*.bmp',
+  './public/assets/images/gallery/*.svg',
+  './public/assets/images/gallery/*.ico',
+  './public/assets/images/gallery/*.dib'
 ]
 const watcher = chokidar.watch(allowedExtensions, {
   persistent: true,
@@ -33,9 +33,10 @@ let emitSwitched = false;
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 })
-app.use('/static/', express.static(__dirname + '/public/css'))
-  .use('/gallery/', express.static(__dirname + '/public/assets/images'))
-  .use('/js/', express.static(__dirname + '/src/scripts'))
+app.use('/static/', express.static(__dirname + '/public/css/'))
+  .use('/gallery/', express.static(__dirname + '/public/assets/images/gallery/'))
+  .use('/textures/', express.static(__dirname + '/public/assets/images/textures/'))
+  .use('/js/', express.static(__dirname + '/src/scripts/'))
 
 http.listen(port, function() {
   console.log('listening on '+port);
@@ -88,6 +89,7 @@ watcher
     data.nrOfImages = data.imageList.length;
     data.nrOfPages = Math.ceil(data.nrOfImages / 10);
     sendUpdatedData(data);
+      console.log('File '+path+' has been changed');
   })
   .on('change', function(path) {
     console.log('File '+path+' has been changed');
